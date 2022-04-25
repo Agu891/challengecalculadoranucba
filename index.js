2e8bf76ce7bd598cdd098493395679b0fdd17ff3
Agustin <agu89.1@gmail.com>
2022-04-25 20:47:56 -0300

    tercer commit

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const numeros = document.querySelectorAll("#botonesbox .btnNum")
 const resultado = document.querySelector("#resultado")
-const operadores = document.getElementsByClassName("btnOperator")
-const borrar1 = document.getElementsByClassName("btnc")
+const operadores = document.querySelectorAll("#botonesbox .btnOperator")
+const borrar1 = document.querySelectorAll("#botonesbox .btnc")
 let resultado2 = document.querySelector("#resultado2")
 let num1 
-let num2  
+let num2 
 
 for(var i = 0; i < numeros.length; i++)
-
 numeros[i].addEventListener("click" , (event)=>{
     let numeroFinal = event.target.value
     resultado.textContent = resultado.textContent + numeroFinal
-   
+   resultado2.textContent = resultado2.textContent + numeroFinal
 })
 
 
@@ -19,71 +18,68 @@ for(var i = 0; i < operadores.length; i++)
 operadores[i].addEventListener("click", (event) =>{
     let operador = event.target.value
     resultado.textContent = resultado.textContent + operador 
-    resultado.value = resultado.textContent
+    
   
 })
 
+// function igual(){
+    
+//     resultado.textContent = eval(resultado.textContent)
+// }
 
-
-function operadorinput () {
-    num1 = parseFloat(resultado.value)
-    
-    for( let i = 0; i< resultado.textContent.length; i++)
-  if(resultado.textContent[i] === "+"){
-     resultado.textContent = ""
-     resultado2.textContent= num1.toString() + "+" 
-     
-  } else if (resultado.textContent[i] === "-"){
-    
-    resultado.textContent = ""
-    resultado2.textContent= num1.toString() + "-"
-} else if (resultado.textContent[i] === "*"){
-    
-    resultado.textContent = ""
-    resultado2.textContent= num1.toString() + "*"
-} else if (resultado.textContent[i] === "/"){
-    
-    resultado.textContent = ""
-    resultado2.textContent= num1.toString() + "/"
-} 
-}
+ function operadorinput () {
+     num1 = parseFloat(resultado.textContent)  
+     for( let i = 0; i< resultado.textContent.length; i++)
+   if(resultado.textContent[i] === "+"){
+      resultado2.textContent = ""
+      resultado.textContent= num1.toString() + "+"    
+   } else if (resultado.textContent[i] === "-"){  
+     resultado2.textContent = ""
+     resultado.textContent= num1.toString() + "-"
+ } else if (resultado.textContent[i] === "*"){  
+     resultado2.textContent  = ""
+     resultado.textContent= num1.toString() + "*"
+ } else if (resultado.textContent[i] === "/"){  
+     resultado2.textContent = ""
+     resultado.textContent= num1.toString() + "/"
+ } 
+ }
   
 
 
 
-function igual (){
-    num2 = parseFloat(resultado.textContent)
-   let suma = num1 + num2
-    let resta = num1 - num2
-    let multiplicacion = num1 * num2
-     let division = num1 / num2 
-    for( let i = 0; i< resultado2.textContent.length; i++)
-     if(resultado2.textContent[i] === "+"){
-        resultado2.textContent= num1.toString() + "+" + num2.toString()
-     return resultado.textContent = suma
-    } else if (resultado2.textContent[i] === "-"){
-        resultado2.textContent= num1.toString() + "-" + num2.toString()
-        return resultado.textContent = resta
-    } else if (resultado2.textContent[i] === "*"){
-        resultado2.textContent= num1.toString() + "*" + num2.toString()
-        return resultado.textContent = multiplicacion
-    }else if (resultado2.textContent[i] === "/"){
-            resultado2.textContent= num1.toString() + "/" + num2.toString()
-            return resultado.textContent = division 
-        }
-    
-}
+ function igual (){
+     num2 = parseFloat(resultado2.textContent)
+    let suma = num1 + num2
+     let resta = num1 - num2
+     let multiplicacion = num1 * num2
+      let division = num1 / num2 
+     for( let i = 0; i< resultado.textContent.length; i++)
+      if(resultado.textContent[i] === "+"){
+        
+      return resultado.textContent = suma
+     } else if (resultado.textContent[i] === "-"){
+       
+         return resultado.textContent = resta 
+     } else if (resultado.textContent[i] === "*"){
+         
+         return resultado.textContent = multiplicacion
+     }else if (resultado.textContent[i] === "/"){
+             return resultado.textContent = division 
+         }  
+ }
 
 
     
 
 
 function borrar (){
-    if(borrar1[0].value === "c"){
-        resultado.innerHTML = ""
+    
+    if(borrar1[0].value === "ac"){
         resultado2.textContent = ""
-    }
-}
+        return resultado.textContent = ""
+        
+    } }
 
 
 
